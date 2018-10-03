0ecaf0dcfd19958937389a6d8d9d9fd5248d9cc5
Malte Schwitters <malte.schwitters@outlook.de>
2018-10-04 01:07:57 +0200

    math functions

--- src/main.c
+++ src/main.c
@@ -5,15 +5,57 @@
 
 int running = 1;
 
-int main(int argc, char **args) {
-	initSensor();
-	initHttp();
+int min(int *array, int arraylen) {
+	int result;
+	int i;
+	for (i=0; i<=arraylen; i++) {
+		if (array[i] < result) {
+			result = array[i];
+		}
+	}
+	return result;
+}
+
+int max(int *array, int arraylen) {
+	int result;
+	int i;
+	for (i=0; i<=arraylen; i++) {
+		if (array[i] > result) {
+			result = array[i];
+		}
+	}
+	return result;
+}
+
+int avg(int *array, int arraylen) {
+	int result;
+	int i;
+	for (i=0; i<=arraylen; i++) {
+		result += array[i];
+	}
+	return result / arraylen;
+}
+
+void loop() {
+	int minute[60];
+	int i;
 	
 	while (running) {
 		//printf("RPM: %d\n", counter);
 		//counter = 0;
-		delay(60 * 1000);
-		//sendToServer();
+		
+		for(i=0; i<=60; i++) {
+			minute[i] = counter;
+			counter = 0;
+			delay(1000);
+			//sendToServer(0, 0, 0);
+		}
 	}
+}
+
+int main(int argc, char **args) {
+	initSensor();
+	initHttp();
+	loop();
 	return 0;
 }
