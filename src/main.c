8ac90ab2086f4cc1f5ca0f638772c73ed43fa2f0
Malte Schwitters <malte.schwitters@outlook.de>
2018-10-03 23:06:00 +0200

    added interrupt

--- src/main.c
+++ src/main.c
@@ -2,15 +2,11 @@
 #include <curl/curl.h>
 #include <wiringPi.h>
 
+#define SENSOR_PIN 0
+
 CURL *curl;
+volatile int counter = 0;
 
-void init() {
-	wiringPiSetup () ;
-	//pinMode (0, INTPUT) ;
-
-	curl_global_init(CURL_GLOBAL_DEFAULT);
-	curl = curl_easy_init();
-}
 
 void cleanup() {
 	curl_easy_cleanup(curl);
@@ -29,15 +25,35 @@ void sendToServer() {
 
 int readSensor() {
 	while(1) {
-		waitForInterrupt (0, -1) ;
+		waitForInterrupt(SENSOR_PIN, -1) ;
 		int value = digitalRead(0);
 		printf("Sensor value: %i\n", value);
 	}
 	return 0;
 }
 
+void sensorInterrupt(void) {
+	counter++;
+}
+
+void init() {
+	wiringPiSetup () ;
+	//pinMode (0, INTPUT) ;
+	if (wiringPiISR(SENSOR_PIN, INT_EDGE_FALLING, &sensorInterrupt) < 0 ) {
+      fprintf (stderr, "Unable to setup ISR: %s\n", strerror (errno));
+      return 1;
+  }
+
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+	curl = curl_easy_init();
+}
+
 int main(int argc, char **args) {
 	init();
-	readSensor();
+	while (1) {
+		printf("RPM: %d\n", counter);
+		counter = 0;
+		delay(60 * 1000);
+	}
 	sendToServer();
 }
